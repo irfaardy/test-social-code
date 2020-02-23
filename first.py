6d10fd58c08f7066c6394476ef4dc7d5eaf6b795
OKITA <61308179+okitasan321@users.noreply.github.com>
2020-02-23 11:21:25 +0700

    Add files via upload

---
+++ first.py
@@ -0,0 +1,25 @@
+# a = 200
+# b = 33
+# if b > a:
+#   print("b is greater than a")
+# elif a == b:
+#   print("a and b are equal")
+# else:
+# 	print("a lebih besar dari b")
+# fruits=["apple", "banana", "cherry"]
+# for f in fruits:
+#  	print(f)
+# import mysql.connector
+a, b = 4, 5;
+c=True;
+d=False;
+if a < b:
+ 	 print(c)
+
+# mydb = mysql.connector.connect(
+#   host="localhost",
+#   user="root",
+#   passwd=""
+# )
+
+# print(mydb) 
